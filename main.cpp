6b6e6fa418938b2d207aad3ad34fc201721b2322
Siddhant18124 <siddhantofficial18@gmail.com>
2022-08-16 15:05:24 +0530

    Testing1

--- main.cpp
+++ main.cpp
@@ -9,5 +9,11 @@ int main(){
     cout << "Kartik is teaching us" <<endl;
     cout << "Hi siddhant" <<endl;
     cout << "Kartik kuch bol le" <<endl;
+
+    int a=5;
+    int b=5;
+
+    cout<<"The result is:  << "(a+b)<<endl;
+
     return 0;
 }
