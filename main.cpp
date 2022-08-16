df074463747666795b5f74ca2cc2453ccfd1db4e
Siddhant18124 <siddhantofficial18@gmail.com>
2022-08-16 14:55:53 +0530

    Hi kartik2

--- main.cpp
+++ main.cpp
@@ -6,5 +6,6 @@ int main(){
     cout << "Hi I am Siddhant" <<endl;
     cout << "Kartik is teaching us" <<endl;
     cout << "Hi I am Rishita" <<endl;
+    cout << "Hi Rishita" <<endl;
     return 0;
 }
